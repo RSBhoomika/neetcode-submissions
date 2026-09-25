59eb6b7318aaf8d23617572d555ce9d9490cf3c5
RSBhoomika <72779921+RSBhoomika@users.noreply.github.com>
2026-09-25 20:57:08 +0530

    Add: number-of-one-bits - submission-1

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-1.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        binary_rep = '{:32b}'.format(n)
+        count = Counter(binary_rep)
+        for num,freq in count.items():
+            if num == '1':
+                return int(freq)
+        return 0
